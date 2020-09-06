e58842be0c6a5d7d8421aff4ce20b8d8a3b5f9e7
Josh Kennedy <joshk@digitalmagic.io>
2020-09-06 14:34:21 -0500

    Fleshed out the schema some more.

--- processor/sql/01_CreateBasicSchema.sql
+++ processor/sql/01_CreateBasicSchema.sql
@@ -8,5 +8,31 @@ CREATE TABLE watchlist.Anime (
 
   -- Japanese (hepburn-ized) title for the anime according to MyAnimeList (the bold text)
   japanese_title TEXT NOT NULL,
+
+  -- URL to the "cover art" (I'm not sure what to call it) from MyAnimeList for the corresponding anime (we're hosting in on S3)
+  art_url TEXT NOT NULL
 ) WITHOUT ROWID; -- We don't want to have a rowid alias, since we'll be using the MAL anime id as our primary key.
 
+-- Create the Todo List table
+CREATE TABLE watchlist.Todo (
+  -- id is an integer that is just a row id (we alias the default rowid)
+  id INTEGER PRIMARY KEY,
+
+  -- anime_id is the foreign key that maps to the Anime table
+  anime_id INTEGER NOT NULL,
+
+  -- position in the todo list
+  position INTEGER NOT NULL UNIQUE
+); -- We use the rowid column in this column (but aliased to id)
+
+-- Create the Pending to Watch table
+CREATE TABLE watchlist.Pending (
+  -- id is an integer that is just a row id (we alias the default rowid)
+  id INTEGER PRIMARY KEY,
+
+  -- anime_id is the foreign key that maps to the Anime table
+  anime_id INTEGER NOT NULL,
+
+  -- position in the pending list
+  position INTEGER NOT NULL UNIQUE
+); -- We use the rowid column in this column (but aliased to id)
